19a251e5c749a8f68ecf7c90e004601b08ad3b75
Sam Sharff <samanthaleec@gmail.com>
2022-10-17 18:04:04 -0400

    adds Person model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,36 +1,29 @@
 require "csv"
 
-csv_text = File.read(Rails.root.join("lib", "seeds", "sr_dev_competitions.csv"))
-competitions_csv = CSV.parse(csv_text, :headers => true, :encoding => "ISO-8859-1")
-
-# team = Team.new({ team_id: "testteam", name: "testteamname", country: "test country", team_type: "testteamtype" })
-# team.save
-
-competitions_csv.each do |row|
-  competition = Competition.new
-  competition.comp_id = row["comp_id"]
-  competition.name = row["name"]
-  competition.team_type = row["team_type"]
-  competition.scope = row["scope"]
-  competition.competition_format = row["competition_format"]
-  competition.country = row["country"]
-  competition.save
-  puts "#{competition.comp_id}, #{competition.name} saved"
-end
-
-# csv_text = File.read(Rails.root.join('lib', 'seeds', 'sr_dev_competitions.csv'))
-# players_csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
-# # team = Team.new({ team_id: "testteam", name: "testteamname", country: "test country", team_type: "testteamtype" })
-# # team.save
+### competitions ###
+# csv_text = File.read(Rails.root.join("lib", "seeds", "sr_dev_competitions.csv"))
+# competitions_csv = CSV.parse(csv_text, :headers => true, :encoding => "ISO-8859-1")
 
 # competitions_csv.each do |row|
 #   competition = Competition.new
-#   competition.comp_id = row['comp_id']
-#   competition.name = row['name']
-#   competition.team_type = row['team_type']
-#   competition.scope = row['scope']
-#   competition.competition_format = row['competition_format']
-#   competition.country = row['country']
+#   competition.comp_id = row["comp_id"]
+#   competition.name = row["name"]
+#   competition.team_type = row["team_type"]
+#   competition.scope = row["scope"]
+#   competition.competition_format = row["competition_format"]
+#   competition.country = row["country"]
 #   competition.save
 #   puts "#{competition.comp_id}, #{competition.name} saved"
 # end
+
+### person ###
+csv_text = File.read(Rails.root.join("lib", "seeds", "sr_dev_people.csv"))
+person_csv = CSV.parse(csv_text, :headers => true, :encoding => "ISO-8859-1")
+
+person_csv.each do |row|
+  person = person.new
+  person.person_id = row["person_id"]
+  person.name = row["name"]
+  person.birth_date = row["birth_date"]
+  puts "#{person.person_id}, #{person.name}  has been saved"
+end
