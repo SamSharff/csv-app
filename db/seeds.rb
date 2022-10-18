8be63a8049aa60f8591c6fd7751aa24fefbd4637
Sam Sharff <samanthaleec@gmail.com>
2022-10-18 13:01:22 -0400

    seeds people CSV to People table

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,29 +4,29 @@ require "csv"
 csv_text = File.read(Rails.root.join("lib", "seeds", "sr_dev_competitions_2.csv"))
 competitions_csv = CSV.parse(csv_text, :headers => true, :encoding => "ISO-8859-1")
 
-competitions_csv.each do |row|
-  competition = Competition.new
-  competition.orig_comp_id = row["orig_comp_id"]
-  competition.name = row["name"]
-  competition.team_type = row["team_type"]
-  competition.scope = row["scope"]
-  competition.competition_format = row["competition_format"]
-  competition.country = row["country"]
-  competition.save
-  puts "#{competition.orig_comp_id}, #{competition.name} saved"
-end
-# ### person ###
-# csv_text = File.read(Rails.root.join("lib", "seeds", "sr_dev_people_2.csv"))
-# people_csv = CSV.parse(csv_text, :headers => true, :encoding => "ISO-8859-1")
-
-# people_csv.each do |row|
-#   person = Person.new
-#   person.orig_person_id = row["orig_person_id"]
-#   person.name = row["name"]
-#   person.birth_date = row["birth_date"]
-#   person.save
-#   puts "#{person.orig_person_id}, #{person.name}  has been saved"
+# competitions_csv.each do |row|
+#   competition = Competition.new
+#   competition.orig_comp_id = row["orig_comp_id"]
+#   competition.name = row["name"]
+#   competition.team_type = row["team_type"]
+#   competition.scope = row["scope"]
+#   competition.competition_format = row["competition_format"]
+#   competition.country = row["country"]
+#   competition.save
+#   puts "#{competition.orig_comp_id}, #{competition.name} saved"
 # end
+### person ###
+csv_text = File.read(Rails.root.join("lib", "seeds", "sr_dev_people_2.csv"))
+people_csv = CSV.parse(csv_text, :headers => true, :encoding => "ISO-8859-1")
+
+people_csv.each do |row|
+  person = Person.new
+  person.orig_person_id = row["orig_person_id"]
+  person.name = row["name"]
+  person.birth_date = row["birth_date"]
+  person.save
+  puts "#{person.orig_person_id}, #{person.name}  has been saved"
+end
 
 # ### stats ###
 # csv_text = File.read(Rails.root.join("lib", "seeds", "sr_dev_people_2.csv"))
