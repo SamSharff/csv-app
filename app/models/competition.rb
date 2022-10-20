af0de3e6de2584605f7834775ccf439819391690
Sam Sharff <samanthaleec@gmail.com>
2022-10-20 16:16:52 -0400

    adds correct has_many belongs_to associations between Competition and Stat tables, reassigning the foreign/primary keys to orig_comp_id

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -1,3 +1,3 @@
 class Competition < ApplicationRecord
-  has_many :stats
+  has_many :stats, foreign_key: :orig_comp_id, primary_key: :orig_comp_id
 end
--- app/models/stat.rb
+++ app/models/stat.rb
@@ -1,5 +1,5 @@
 class Stat < ApplicationRecord
   belongs_to :person, foreign_key: :orig_person_id, primary_key: :orig_person_id
   belongs_to :team, foreign_key: :orig_team_id, primary_key: :orig_team_id
-  belongs_to :competition
+  belongs_to :competition, foreign_key: :orig_comp_id, primary_key: :orig_comp_id
 end
