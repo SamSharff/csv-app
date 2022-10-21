6a890efb323287324a0efcaf1e7ee24a629c999f
Sam Sharff <samanthaleec@gmail.com>
2022-10-21 10:47:13 -0400

    adds self.ronaldo and practices with age method on Person model

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,7 +1,17 @@
 class Person < ApplicationRecord
   has_many :stats, foreign_key: :orig_person_id, primary_key: :orig_person_id
 
+  def self.ronaldo
+    # Person.find_by(orig_person_id: "dea698d9")
+    Person.where(orig_person_id: "dea698d9")
+    # Person.where(orig_person_id: "dea698d9").select(:id, :first_name).take
+  end
+
   # def age
   #   Date.new() Date.today.year - birth_date.year
   # end
+
+  def age
+    Date.today.year - birth_date.year
+  end
 end
